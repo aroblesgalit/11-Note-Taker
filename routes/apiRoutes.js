3995074470525123950991c85a78eb003fb26fa9
Alvin Galit <aroblesgalit@gmail.com>
2020-04-16 21:48:07 -0500

    Added a line of code for serving static files with express

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,20 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var noteData = require("../db/db");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var db = require("../db/db");
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function(req, res) {
-    res.json(noteData);
+    res.json(db);
   });
-
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/notes", function(req, res) {
-    
-      noteData.push(req.body);
+      db.push(req.body);
       res.json(true);
   });
+
+  // app.delete("/api/notes/:id", function(req, res) {
+  //   var deleteNote = db[id];
+  //   delete deleteNote;
+  //   res.end("Deleted note: \n" + JSON.stringify(deleteNote, null, 4));
+  // })
 
 };
